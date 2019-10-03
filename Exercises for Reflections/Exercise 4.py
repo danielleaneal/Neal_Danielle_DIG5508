11a10ee83b2385272c3b7037faff0910616d8fc3
sthephmaldonado <54649984+sthephmaldonado@users.noreply.github.com>
2019-10-02 22:24:15 -0400

    Collaborating once more

--- Exercises for Reflections/Exercise 4.py
+++ Exercises for Reflections/Exercise 4.py
@@ -8,4 +8,4 @@ print("Hello World. It's Danielle Ashton Neal. Nice to meet you.")
 print("Hello Danielle, I am collaborating for this excersice.")
 
 #%%
-print("Hello again, It is Sthephany Maldonado.")
+print("Hello again, It is Sthephany Maldonado!")

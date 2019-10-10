620b9f5a35446b9533dc0512cab9dc76769adc17
danielleaneal <54646998+danielleaneal@users.noreply.github.com>
2019-10-09 21:41:02 -0400

    Reflection

--- Exercises-for-Reflections/Exercise-6.py
+++ Exercises-for-Reflections/Exercise-6.py
@@ -1,2 +1,17 @@
 #Week 6 Exercise for Reflection 10/9/19
+Removing Vowels
+
+#%%
+
+def devowel(randomthingminusvowels):
+    List = list(randomthingminusvowels)
+    vowels=["a","e","i","o","u"]
+    for letter in List:
+        if letter.lower () in vowels:
+            List.remove(letter)
+    randomthingminusvowels = ''.join(List)
+    return randomthingminusvowels
+
+ 
+
 

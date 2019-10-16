25f1073d9e172220154b49ce544b59dfd655a70a
Danielle <danielleaneal@aol.com>
2019-10-16 17:28:19 -0400

    Free Project 1 Work

--- Project-1/Project1.py
+++ Project-1/Project1.py
@@ -1,1 +1,19 @@
 #Project1 Danielle Neal
+#My Attempt at Free Project 7-3 from the Textbook
+
+#%%
+print("Hello World. I'm trying to tell you Hello World an unbounded number of times!")
+
+#%% 
+for i in range (5):
+    print("Hello World. I'm trying to tell you Hello World an unbounded number of times!")
+
+#%%
+while True:
+    print("everything is ok.")
+
+#%%
+#This program represents what my mind has been doing while trying to figure 
+#out how to code. Welcome to my consciousness. Everything's fine here.
+
+#%%

d835f5020cc5c302a787bb6dfc50ab2cb7f07e7e
danielleaneal <54646998+danielleaneal@users.noreply.github.com>
2019-10-16 21:06:33 -0400

    New Work

--- Project-1/Project1.py
+++ Project-1/Project1.py
@@ -1,19 +1,43 @@
 #Project1 Danielle Neal
-#My Attempt at Free Project 7-3 from the Textbook
+#My Attempt at Free Project 7-1 from the Textbook
 
-#%%
-print("Hello World. I'm trying to tell you Hello World an unbounded number of times!")
+#!/usr/bin/python
 
-#%% 
-for i in range (5):
-    print("Hello World. I'm trying to tell you Hello World an unbounded number of times!")
+# Stochastic Texts, copyright (c) 2014 Nick Montfort <nickm@nickm.com>
+# Original by Theo Lutz, 1959; translation by Helen MacCormack
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+# SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+# IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+# Updated 31 May 2018 to add compatibility with Python 3 (Python 2 still works)
 
-#%%
-while True:
-    print("everything is ok.")
+from random import choice
 
-#%%
-#This program represents what my mind has been doing while trying to figure 
-#out how to code. Welcome to my consciousness. Everything's fine here.
+subjects = ['COUNT', 'STRANGER', 'LOOK', 'CHURCH', 'CASTLE', 'PICTURE',
+            'EYE', 'VILLAGE', 'TOWER', 'FARMER', 'WAY', 'GUEST', 'DAY',
+            'HOUSE', 'TABLE', 'LABOURER']
+predicates = ['OPEN', 'SILENT', 'STRONG', 'GOOD', 'NARROW', 'NEAR',
+              'NEW', 'QUIET', 'FAR', 'DEEP', 'LATE', 'DARK', 'FREE',
+              'LARGE', 'OLD', 'ANGRY']
+conjunctions = [' AND ', ' OR ', ' THEREFORE ', '. ', '. ', '. ', '. ', '. ']
+operators = ['A', 'EVERY', 'NO', 'NOT EVERY']
 
-#%%
+def phrase():
+    text = choice(operators) + ' ' + choice(subjects)
+    if text == 'A EYE':
+        text = 'AN EYE'
+    return text + ' IS '
+
+print('')
+print(phrase() + choice(predicates) + choice(conjunctions) +
+       phrase() + choice(predicates) + '.')
+print('')
